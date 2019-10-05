727b685c219f85d20775f85982c3db746b3bab99
Brandon Cate <brandoncate95@gmail.com>
2019-10-05 05:22:53 -0700

    init firebase, small changes for hosting

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,15 @@ import SignUp from './components/AuthFlow/SignUp';
 import Home from './components/Home';
 import SwitchWithSlide from './components/Slider/SwitchWithSlide';
 
+import * as ROUTES from './constants/routes'
+import SearchPage from './components/SearchPage';
+
 function App() {
   return (
     <Router>
       <SwitchWithSlide>
         <Route exact path="/" component={Home} />
+        <Route path="/search" component={SearchPage} />
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
       </SwitchWithSlide>
